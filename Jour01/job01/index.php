ba3ee1fb818071414feb09f2c931c3f7850ebeb6
SpectrePhantom <adel-zammit@users.noreply.github.com>
2020-05-27 03:43:25 +0200

    Runtrack2

--- Jour01/job01/index.php
+++ Jour01/job01/index.php
@@ -1,22 +1,23 @@
-<?php
+<?php 
 
-$str = "Hello LaPlateforme";
-    echo $str;
+$str = 'Hello LaPlateforme';
+    echo "$str";
 
-$str2 = "Vive";
-$str3 = "!";
-    echo $str2, ' LaPlateforme ' .$str3;
+$str2 = 'Vive';
+$str3 = '!';
+    echo "$str2 LaPlateforme $str3";
 
-$val = 6;
-    echo $val;
+$val = '6';
+    echo "$val";
 
-$val += 4;
-    echo $val;
+$val += '4';
+    echo "$val";
 
 $myBool = true;
-    var_dump ($myBool);
+    echo "$myBool";
 
-$myBool = false;
-    var_dump ($myBool);
-    
+$myBool += false;
+    echo "$myBool"
+
+
 ?>
