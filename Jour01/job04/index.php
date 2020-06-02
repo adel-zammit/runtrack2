a1e3c5eeb723fc1c7df94cea17a3ed11eaf09d9b
SpectrePhantom <adel-zammit@users.noreply.github.com>
2020-06-02 12:04:37 +0200

    Runtrack2

---
+++ Jour01/job04/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$str = "Hello LaPlateforme";
+    echo $str;
+
+$str2 = "Vive";
+$str3 = "!";
+    echo $str2, ' LaPlateforme ' .$str3;
+
+$val = 6;
+    echo $val;
+
+$val += 4;
+    echo $val;
+
+$myBool = true;
+    var_dump ($myBool);
+
+$myBool = false;
+    var_dump ($myBool);
+    
+?>
